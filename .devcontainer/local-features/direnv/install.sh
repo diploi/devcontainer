494232b40361564912a257e48b152e0ea0330b92
Tuomas <me@tuomas.id>
2025-08-28 17:48:55 +0300

    Switch python to use uv

--- .devcontainer/local-features/direnv/install.sh
+++ .devcontainer/local-features/direnv/install.sh
@@ -6,4 +6,4 @@ sudo -iu $_REMOTE_USER <<EOF
     curl -sfL https://direnv.net/install.sh | bash
 EOF
 
-sed -i 's/^plugins=(\(.*\))/plugins=(\1 direnv)/' ~/.zshrc
+sed -i 's/^plugins=(\(.*\))/plugins=(\1 direnv)/' /home/$_CONTAINER_USER/.zshrc
