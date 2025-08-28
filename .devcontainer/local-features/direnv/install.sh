e7100fe9e48909357becd447fdc48cfb412a5762
Tuomas <me@tuomas.id>
2025-08-28 16:16:21 +0300

    Add direnv as an Oh My Zsh plugin

--- .devcontainer/local-features/direnv/install.sh
+++ .devcontainer/local-features/direnv/install.sh
@@ -6,10 +6,4 @@ sudo -iu $_REMOTE_USER <<EOF
     curl -sfL https://direnv.net/install.sh | bash
 EOF
 
-cat <<EOF >> /home/$_CONTAINER_USER/.bashrc
-eval "$(direnv hook bash)"
-EOF
-
-cat <<EOF >> /home/$_CONTAINER_USER/.zshrc
-eval "$(direnv hook zsh)"
-EOF
+sed -i 's/^plugins=(\(.*\))/plugins=(\1 direnv)/' ~/.zshrc
