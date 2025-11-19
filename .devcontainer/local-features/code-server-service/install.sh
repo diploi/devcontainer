a1748d0067b742666d7aa9b8951f9eb7598a9439
giaongo <83873333+giaongo@users.noreply.github.com>
2025-11-19 13:32:34 +0200

    Test out giaongo in code-server-service

--- .devcontainer/local-features/code-server-service/install.sh
+++ .devcontainer/local-features/code-server-service/install.sh
@@ -6,8 +6,8 @@ cat <<EOT >> /etc/supervisord.conf
 ; Added by code-server-service feature
 [program:code-server]
 directory=/app
-user=$_CONTAINER_USER
-environment=HOME="/home/$_CONTAINER_USER"
+user=$USERNAME
+environment=HOME="/home/$USERNAME"
 command=/usr/local/bin/code-server-entrypoint
 autostart=true
 autorestart=true
