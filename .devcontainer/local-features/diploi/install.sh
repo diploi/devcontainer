24f2ea3cf43cb3a78e588d4f32556d2d4a2c2378
giaongo <83873333+giaongo@users.noreply.github.com>
2026-05-19 16:24:00 +0300

    Fix permission issue in .local/state

--- .devcontainer/local-features/diploi/install.sh
+++ .devcontainer/local-features/diploi/install.sh
@@ -3,8 +3,8 @@
 set -eax
 
 # Fix permissions on the share folder
-mkdir -p /home/$_CONTAINER_USER/.local/share
-chown $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.local/share
+mkdir -p /home/$_CONTAINER_USER/.local/share /home/$_CONTAINER_USER/.local/state /home/$_CONTAINER_USER/.config
+chown -R $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.local /home/$_CONTAINER_USER/.config
 
 # Create the diploi-credential-helper
 cat > /usr/local/bin/diploi-credential-helper <<'EOT'
