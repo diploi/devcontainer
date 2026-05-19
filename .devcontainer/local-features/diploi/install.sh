c09e9faf0508d23e19d02c514eec31526a9442e2
giaongo <83873333+giaongo@users.noreply.github.com>
2026-05-19 14:40:47 +0300

    Fix pnpm config

--- .devcontainer/local-features/diploi/install.sh
+++ .devcontainer/local-features/diploi/install.sh
@@ -4,7 +4,7 @@ set -eax
 
 # Fix permissions on the share folder
 mkdir -p /home/$_CONTAINER_USER/.local/share
-chown -R $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.local
+chown $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.local/share
 
 # Create the diploi-credential-helper
 cat > /usr/local/bin/diploi-credential-helper <<'EOT'
@@ -147,7 +147,7 @@ mkdir -p /home/$_CONTAINER_USER/.pnpm-store
 chown $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.pnpm-store
 mkdir -p /home/$_CONTAINER_USER/.local/share/pnpm/bin
 chown -R $_CONTAINER_USER:$_CONTAINER_USER /home/$_CONTAINER_USER/.local/share/pnpm
-sudo -u $_CONTAINER_USER -H zsh -lc "export PNPM_HOME=/home/$_CONTAINER_USER/.local/share/pnpm && export PATH=\"\$PNPM_HOME/bin:\$PNPM_HOME:\$PATH\" && COREPACK_ENABLE_DOWNLOAD_PROMPT=0 . /usr/local/share/nvm/nvm.sh && corepack enable pnpm && SHELL=/usr/bin/zsh pnpm setup && pnpm config set node-linker=hoisted --global && pnpm config set store-dir /home/$_CONTAINER_USER/.pnpm-store --global"
+sudo -u $_CONTAINER_USER -H zsh -lc "export PNPM_HOME=/home/$_CONTAINER_USER/.local/share/pnpm && export PATH=\"\$PNPM_HOME/bin:\$PNPM_HOME:\$PATH\" && COREPACK_ENABLE_DOWNLOAD_PROMPT=0 . /usr/local/share/nvm/nvm.sh && corepack enable pnpm && SHELL=/usr/bin/zsh pnpm setup && pnpm config set store-dir /home/$_CONTAINER_USER/.pnpm-store --global"
 
 echo "Creating Continue configuration file..."
 mkdir -p /home/$_CONTAINER_USER/.continue
